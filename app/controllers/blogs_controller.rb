e1e55cf5ac0e4815e0835bbed17c3ffa8d84ddf2
junpei onizawa <onizawajunpei4@gmail.com>
2019-02-09 18:40:00 +0900

    first commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,16 @@
+class BlogsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+  def top
+  	
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'blogs/show'
+  get 'blogs/index'
+  get 'blogs/new'
+  get 'blogs/edit'
+  get '/' => 'blogs#top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
