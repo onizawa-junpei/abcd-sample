1affcc24b57120d0dee524611599ea0e81f704c1
junpei onizawa <onizawajunpei4@gmail.com>
2019-02-10 14:06:01 +0900

    oni2

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'blogs/new'
   get 'blogs/edit'
   get '/' => 'blogs#top'
+  get 'top' => 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
